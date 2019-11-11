c66a531a43d5369d7142eb95c63e8bce3a9ce410
Rafael Nogales <xrnogales@gmail.com>
2019-11-11 23:28:12 +0100

    remove unused file

--- amazon/scraper_test.py
+++ amazon/scraper_test.py
@@ -6,6 +6,7 @@ scraper = AmazonScraper()
 result = scraper.scrape(url)
 result.to_dict()
 
+
 def test_scrape_amazon_product_url():
     url = 'https://www.amazon.es/gp/product/B06XFHQGB9?pf_rd_p=44ae21af-cb43-440e-bcab-82e6a5324e39&pf_rd_r=8QV60VAX6GCJHW92RKH9'
     scraper = AmazonScraper()
